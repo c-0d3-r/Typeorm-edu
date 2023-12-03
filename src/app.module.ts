2a75fec7294aee7653d880de34568ba029dd8668
Aleksandr Schemelev <aleksandr.schemelev>
2023-12-03 20:42:50 +0300

    feat: add typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
-import { Module }        from '@nestjs/common';
-import { ConfigModule }  from '@nestjs/config';
+import { Module }                            from '@nestjs/common';
+import { ConfigModule, ConfigService }       from '@nestjs/config';
+import { TypeOrmModule }                     from '@nestjs/typeorm';
 
-import { AppController } from './app.controller';
+import { AppController }                     from './app.controller';
+import databaseConf, { type DatabaseConfig } from './config/db.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [databaseConf],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory(config: ConfigService<DatabaseConfig>) {
+        return config.get('database', {
+          infer: true,
+        });
+      },
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
