af397476c09b456d20c26810347811b1bda938bf
Aleksandr Schemelev <aleksandr.schemelev>
2024-01-18 00:33:56 +0300

    feat: add event handlers

--- src/modules/users/application/use-cases/create-user.use-case.ts
+++ src/modules/users/application/use-cases/create-user.use-case.ts
@@ -1,7 +1,8 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
 
-import type { User }                       from '../../domain/entities/user.entity';
-import { UsersRepository }                 from '../../infrastructure/repositories/users.repository';
+import type { User }                                 from '../../domain/entities/user.entity';
+import { UsersRepository }                           from '../../infrastructure/repositories/users.repository';
+import { UserCreatedEvent }                          from '../events/user-created.event';
 
 export class CreateUserCommand {
   public constructor(
@@ -15,13 +16,20 @@ export class CreateUserCommand {
 export class CreateUserUseCase
   implements ICommandHandler<CreateUserCommand, User>
 {
-  public constructor(public readonly usersRepository: UsersRepository) {}
+  public constructor(
+    public readonly usersRepository: UsersRepository,
+    private readonly eventBus: EventBus,
+  ) {}
 
   public async execute(command: CreateUserCommand): Promise<User> {
-    return this.usersRepository.save({
+    const newUser = await this.usersRepository.save({
       username: command.username,
       email: command.email,
       tel: command.tel,
     });
+
+    await this.eventBus.publish(new UserCreatedEvent(newUser.username));
+
+    return newUser;
   }
 }
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,17 +1,24 @@
-import { Module }                                     from '@nestjs/common';
-import { CqrsModule, ICommandHandler, IQueryHandler } from '@nestjs/cqrs';
-import { TypeOrmModule }                              from '@nestjs/typeorm';
+import { Module }                       from '@nestjs/common';
+import {
+  CqrsModule,
+  ICommandHandler,
+  IEventHandler,
+  IQueryHandler,
+} from '@nestjs/cqrs';
+import { TypeOrmModule }                from '@nestjs/typeorm';
 
-import { User }                                       from 'src/modules/users/domain/entities/user.entity';
+import { User }                         from 'src/modules/users/domain/entities/user.entity';
 
-import { GetUserQuery }                               from './application/queries/get-user.query';
-import { CreateUserUseCase }                          from './application/use-cases/create-user.use-case';
-import { UsersControllers }                           from './controllers/users.controller';
-import { Account }                                    from './domain/entities/account.entity';
-import { UserProductsRepository }                     from './infrastructure/repositories/user-products.repository';
-import { UsersQueryRepository }                       from './infrastructure/repositories/users.query.repository';
-import { UsersRepository }                            from './infrastructure/repositories/users.repository';
-import { Product }                                    from '../products/domain/entities/product.entity';
+import { SendWelcomeEmailEventHandler } from './application/event-handlers/send-welcome-email.event-handler';
+import { UpdateStatisticsEventHandler } from './application/event-handlers/update-statistics.event-handler';
+import { GetUserQuery }                 from './application/queries/get-user.query';
+import { CreateUserUseCase }            from './application/use-cases/create-user.use-case';
+import { UsersControllers }             from './controllers/users.controller';
+import { Account }                      from './domain/entities/account.entity';
+import { UserProductsRepository }       from './infrastructure/repositories/user-products.repository';
+import { UsersQueryRepository }         from './infrastructure/repositories/users.query.repository';
+import { UsersRepository }              from './infrastructure/repositories/users.repository';
+import { Product }                      from '../products/domain/entities/product.entity';
 
 const useCases: { new (...args: any): ICommandHandler<any, any> }[] = [
   CreateUserUseCase,
@@ -19,6 +26,11 @@ const useCases: { new (...args: any): ICommandHandler<any, any> }[] = [
 
 const queries: { new (...args: any): IQueryHandler<any, any> }[] = [
   GetUserQuery,
+];
+
+const eventHandlers: { new (...args: any): IEventHandler<any> }[] = [
+  UpdateStatisticsEventHandler,
+  SendWelcomeEmailEventHandler,
 ];
 
 @Module({
@@ -30,6 +42,7 @@ const queries: { new (...args: any): IQueryHandler<any, any> }[] = [
     UserProductsRepository,
     ...useCases,
     ...queries,
+    ...eventHandlers,
   ],
 })
 export class UsersModule {}
